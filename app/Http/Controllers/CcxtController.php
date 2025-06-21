fda313d718eca9e581c05ffd369300602d12f81c
adimiupprix <miuprix@gmail.com>
2025-06-21 13:03:58 +0700

    binance apa sih....

--- app/Http/Controllers/CcxtController.php
+++ app/Http/Controllers/CcxtController.php
@@ -11,7 +11,26 @@ class CcxtController extends Controller
     public function listExchanges(): JsonResponse
     {
         $now = Carbon::now('Asia/Jakarta');
-        dd($now->toDateTimeString());
+
+        $exchange_id = 'binance';
+        $exchange_class = "\\ccxt\\{$exchange_id}";
+        if (!class_exists($exchange_class)) {
+            return response()->json(['error' => 'Exchange not found'], 404);
+        }
+
+        $exchange = new $exchange_class([
+            'enableRateLimit' => true,
+            'options' => [
+                'adjustForTimeDifference' => true,
+            ],
+        ]);
+        $exchange->load_markets();
+        $exchange->fetch_time();
+        $exchange->set_sandbox_mode(true);
+        $exchange->set_time_difference($now->timestamp * 1000 - $exchange->fetch_time());
+        $exchange->set_sandbox_mode(false);
+
+
         return response()->json([
             'exchanges' => Exchange::$exchanges,
         ]);
