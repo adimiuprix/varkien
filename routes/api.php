d2ef4d52b042734423dcb4aaf40b0e1bf3da1259
adimiuprix <miuprix@gmail.com>
2025-11-25 03:32:41 +0700

    fix route

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TradeController;
-use App\Http\Controllers\PustakaController;
-
-Route::apiResource('pustaka', PustakaController::class);
-Route::post('buang', [PustakaController::class, 'buangData']);
-Route::post('buang', [PustakaController::class, 'buangData']);
 
 // Public endpoints (tanpa middleware)
 Route::prefix('trade')->group(function () {
+
+    // Handle Signal
+    Route::get('/signal', [TradeController::class, 'handleSignal'])
+        ->name('trade.signal');
     
     // Status check
     Route::get('/status', [TradeController::class, 'status'])
